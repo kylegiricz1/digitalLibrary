9b0eb2cee71779f6db6cb58358c6f41ac408aa55
kylegiricz1 <159865309+kylegiricz1@users.noreply.github.com>
2024-02-12 21:42:38 -0600

    Create DigitalLibrary.java
    DigitalLibrary.java created

---
+++ DigitalLibrary.java
@@ -0,0 +1,28 @@
+package DigitalLibrary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DigitalLibrary {
+    private List<Book> books;
+
+    public DigitalLibrary() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books in the library.");
+        } else {
+            System.out.println("Books available in the library:");
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        }
+    }
+}
+
