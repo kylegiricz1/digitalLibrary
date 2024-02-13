88cd0749c1a24105a6e2b9b7d9e97d0b90486ddc
kylegiricz1 <159865309+kylegiricz1@users.noreply.github.com>
2024-02-12 21:42:47 -0600

    Update Main.java
    Main.java Created

---
+++ Main.java
@@ -0,0 +1,15 @@
+package DigitalLibrary;
+
+public class Main {
+    public static void main(String[] args) {
+        DigitalLibrary library = new DigitalLibrary();
+
+        // Adding books to the library
+        library.addBook(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925));
+        library.addBook(new Book("To Kill a Mockingbird", "Harper Lee", 1960));
+        library.addBook(new Book("1984", "George Orwell", 1949));
+
+        // Displaying books in the library
+        library.displayBooks();
+    }
+}
